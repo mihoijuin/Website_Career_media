5783f05c448228c484f96951f482adbbce7b1cf0
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-07-21 20:03:51 +0900

    docコメントを追加

--- functions.php
+++ functions.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Nandedemodakara functions and definitions
+ *
+ * @package WordPress
+ * @subpackage Nandedemodakara
+ * @since 1.0
+ */
+
+/**
+ * Nandedemodakara only works in WordPress 4.7 or later.
+ */
 
 add_theme_support('menus');
 
