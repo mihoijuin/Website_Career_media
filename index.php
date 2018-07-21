f841e41ed27b2e74a4bbc945add4d69be223649f
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-07-21 18:44:52 +0900

    記事一覧部分のループを作成

--- index.php
+++ index.php
@@ -4,106 +4,34 @@
       <div class="main-wrapper">
         <img class="heading" src="<?php echo get_template_directory_uri(); ?>/images/post-title.svg" alt="">
         <div class="posts">
+          <?php
+          if (have_posts()):
+            while(have_posts()):
+              the_post();
+          ?>
+
           <article class="post">
-            <a href="single.html">
+            <a href="<?php the_permalink(); ?>">
               <header>
-                <h3 class="post-title">記事タイトル</h3>
-                <p class="post-category">カテゴリ</p>
+                <h3 class="post-title"><?php the_title(); ?></h3>
+                <p class="post-category"><?php the_category(','); ?></p>
               </header>
-              <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-              <p class="released-date">2018/07/08</p>
+              <p class="post-txt"><?php the_excerpt(); ?></p>
+              <p class="released-date"><?php echo get_the_date(); ?></p>
             </a>
           </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
+
+          <?php
+            endwhile;
+          else:
+          ?>
+
+          <p>記事はまだないみたいだ。</p>
+
+          <?php
+          endif;
+          ?>
+
           <nav class="page-nav">
             <ul>
               <li>1</li>
@@ -116,35 +44,4 @@
       </div>
     </main>
 
-    <!-- footer -->
-    <footer>
-      <!-- copyrightをflexの範囲外に置くためにfooter-containerでcopyright以外を囲む -->
-      <div class="footer-container">
-        <section class="footer-logo">
-          <a href="page.html">
-            <img class="footer-icon" src="<?php echo get_template_directory_uri(); ?>/images/Porin_icon.jpg" alt="" width="120" height="120">
-          </a>
-          <h1><img class="footer-title" src="<?php echo get_template_directory_uri(); ?>/images/nandedemodakara_title.svg" alt=""></h1>
-          <p>
-            ニートとHR業界サラリーマンが対話し<br>
-            「なんで？」「でも〜」「だから〜」が飛び交う場所
-          </p>
-        </section>
-        <div class="social">
-          <ul>
-            <li><a href="https://twitter.com/iju_miho" target="_blank"><span class="fab fa-twitter"></span></a></li>
-            <li><a href="#" target="_blank"><span class="fas fa-rss"></span></a></li>
-          </ul>
-        </div>
-        <nav class="footer-nav">
-          <ul>
-            <li><a href="page.html">トップ</a></li>
-            <li><a href="index.html">記事一覧</a></li>
-            <li><a href="profile.html">管理人</a></li>
-          </ul>
-        </nav>
-      </div>
-      <p class="copyright"><small>Copyright(c)2018 nandedemodakara All Rights Reserved.</small></p>
-    </footer>
-  </body>
-</html>
+<?php get_footer(); ?>
