ff46d3d5c1bf37df2d5dc868b4f7285dacbd0a43
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-07-22 23:34:52 +0900

    フッターを読み込み

--- single.php
+++ single.php
@@ -68,35 +68,4 @@
       </div>
     </div>
 
-    <!-- footer -->
-    <footer>
-      <!-- copyrightをflexの範囲外に置くためにfooter-containerでcopyright以外を囲む -->
-      <div class="footer-container">
-        <section class="footer-logo">
-          <a href="page.html">
-            <img class="footer-icon" src="./images/Porin_icon.jpg" alt="" width="120" height="120">
-          </a>
-          <h1><img class="footer-title" src="./images/nandedemodakara_title.svg" alt=""></h1>
-          <p>
-            ニートとHR業界サラリーマンが対話し<br>
-            「なんで？」「でも〜」「だから〜」が飛び交う場所
-          </p>
-        </section>
-        <div class="social">
-          <ul>
-            <li><a href="https://twitter.com/iju_miho" target="_blank"><span class="fab fa-twitter"></span></a></li>
-            <li><a href="#" target="_blank"><span class="fas fa-rss"></span></a></li>
-          </ul>
-        </div>
-        <nav class="footer-nav">
-          <ul>
-            <li><a href="page.html">トップ</a></li>
-            <li><a href="index.html">記事一覧</a></li>
-            <li><a href="profile.html">管理人</a></li>
-          </ul>
-        </nav>
-      </div>
-      <p class="copyright"><small>Copyright(c)2018 nandedemodakara All Rights Reserved.</small></p>
-    </footer>
-  </body>
-</html>
+<?php get_footer(); ?>
