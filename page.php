5ed91ff811c0da2658975d1eaeacb7f9bba81d29
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-07-22 19:36:30 +0900

    スペースをタブに修正

--- page.php
+++ page.php
@@ -10,133 +10,101 @@
 
 ?>
 <?php get_header(); ?>
-    <!-- main -->
-    <main>
-      <div class="main-wrapper">
-        <img class="heading" src="./images/popular-title.svg" alt="">
-        <div class="popular-posts">
-          <article class="popular-post">
-            <a href="single.html">
-              <header>
-                <h3 class="popular-title">記事タイトル</h3>
-                <p class="popular-category">カテゴリ</p>
-              </header>
-              <p>記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-              <p class="released-date">2018/07/08</p>
-            </a>
-          </article>
-          <article class="popular-post">
-              <a href="single.html">
-                <header>
-                  <h3 class="popular-title">記事タイトル</h3>
-                  <p class="popular-category">カテゴリ</p>
-                </header>
-                <p>記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <article class="popular-post">
-              <a href="single.html">
-                <header>
-                  <h3 class="popular-title">記事タイトル</h3>
-                  <p class="popular-category">カテゴリ</p>
-                </header>
-                <p>記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-        </div>
+		<!-- main -->
+		<main>
+			<div class="main-wrapper">
+				<img class="heading" src="./images/popular-title.svg" alt="">
+				<div class="popular-posts">
+					<article class="popular-post">
+						<a href="single.html">
+							<header>
+								<h3 class="popular-title">記事タイトル</h3>
+								<p class="popular-category">カテゴリ</p>
+							</header>
+							<p>記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
+							<p class="released-date">2018/07/08</p>
+						</a>
+					</article>
+					<article class="popular-post">
+						<a href="single.html">
+							<header>
+								<h3 class="popular-title">記事タイトル</h3>
+								<p class="popular-category">カテゴリ</p>
+							</header>
+							<p>記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
+							<p class="released-date">2018/07/08</p>
+							</a>
+					</article>
+					<article class="popular-post">
+						<a href="single.html">
+							<header>
+								<h3 class="popular-title">記事タイトル</h3>
+								<p class="popular-category">カテゴリ</p>
+							</header>
+							<p>記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
+							<p class="released-date">2018/07/08</p>
+						</a>
+					</article>
+				</div>
 
-        <img class="heading" src="./images/post-title.svg" alt="">
-        <div class="posts">
-          <article class="post">
-            <a href="single.html">
-              <header>
-                <h3 class="post-title">記事タイトル</h3>
-                <p class="post-category">カテゴリ</p>
-              </header>
-              <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-              <p class="released-date">2018/07/08</p>
-            </a>
-          </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <article class="post">
-              <a href="single.html">
-                <header>
-                  <h3 class="post-title">記事タイトル</h3>
-                  <p class="post-category">カテゴリ</p>
-                </header>
-                <p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
-                <p class="released-date">2018/07/08</p>
-              </a>
-          </article>
-          <nav class="more-nav">
-            <ul>
-              <li><a href="index.html">More...</a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </main>
+				<img class="heading" src="./images/post-title.svg" alt="">
+				<div class="posts">
+					<article class="post">
+						<a href="single.html">
+							<header>
+								<h3 class="post-title">記事タイトル</h3>
+								<p class="post-category">カテゴリ</p>
+							</header>
+							<p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
+							<p class="released-date">2018/07/08</p>
+						</a>
+					</article>
+					<article class="post">
+						<a href="single.html">
+							<header>
+								<h3 class="post-title">記事タイトル</h3>
+								<p class="post-category">カテゴリ</p>
+							</header>
+							<p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
+							<p class="released-date">2018/07/08</p>
+						</a>
+					</article>
+					<article class="post">
+						<a href="single.html">
+							<header>
+								<h3 class="post-title">記事タイトル</h3>
+								<p class="post-category">カテゴリ</p>
+							</header>
+							<p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
+							<p class="released-date">2018/07/08</p>
+						</a>
+					</article>
+					<article class="post">
+						<a href="single.html">
+							<header>
+								<h3 class="post-title">記事タイトル</h3>
+								<p class="post-category">カテゴリ</p>
+							</header>
+							<p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
+							<p class="released-date">2018/07/08</p>
+						</a>
+					</article>
+					<article class="post">
+						<a href="single.html">
+							<header>
+								<h3 class="post-title">記事タイトル</h3>
+								<p class="post-category">カテゴリ</p>
+							</header>
+							<p class="post-txt">記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
+							<p class="released-date">2018/07/08</p>
+						</a>
+					</article>
+					<nav class="more-nav">
+						<ul>
+							<li><a href="index.html">More...</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</main>
 
-    <!-- footer -->
-    <footer>
-      <!-- copyrightをflexの範囲外に置くためにfooter-containerでcopyright以外を囲む -->
-      <div class="footer-container">
-        <section class="footer-logo">
-          <a href="page.html">
-            <img class="footer-icon" src="./images/Porin_icon.jpg" alt="" width="120" height="120">
-          </a>
-          <h1><img class="footer-title" src="./images/nandedemodakara_title.svg" alt=""></h1>
-          <p>
-            ニートとHR業界サラリーマンが対話し<br>
-            「なんで？」「でも〜」「だから〜」が飛び交う場所
-          </p>
-        </section>
-        <div class="social">
-          <ul>
-            <li><a href="https://twitter.com/iju_miho" target="_blank"><span class="fab fa-twitter"></span></a></li>
-            <li><a href="#" target="_blank"><span class="fas fa-rss"></span></a></li>
-          </ul>
-        </div>
-        <nav class="footer-nav">
-          <ul>
-            <li><a href="page.html">トップ</a></li>
-            <li><a href="index.html">記事一覧</a></li>
-            <li><a href="profile.html">管理人</a></li>
-          </ul>
-        </nav>
-      </div>
-      <p class="copyright"><small>Copyright(c)2018 nandedemodakara All Rights Reserved.</small></p>
-    </footer>
-  </body>
- </html>
