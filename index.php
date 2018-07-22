36e8c4ad9655295e6d0562022b6b804ba8ce721e
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-07-22 17:12:23 +0900

    phpタグ周りのスペースやタブを修正

--- index.php
+++ index.php
@@ -16,32 +16,32 @@
 				<img class="heading" src="<?php echo get_template_directory_uri(); ?>/images/post-title.svg" alt="">
 				<div class="posts">
 					<?php
-						if (have_posts()):
-							while(have_posts()):
+					if ( have_posts() ) :
+						while ( have_posts() ) :
 								the_post();
-					?>
+							?>
 
 					<article class="post">
 						<a href="<?php the_permalink(); ?>">
 							<header>
 								<h3 class="post-title"><?php the_title(); ?></h3>
-								<p class="post-category"><?php the_category(','); ?></p>
+								<p class="post-category"><?php the_category( ',' ); ?></p>
 							</header>
 							<p class="post-txt"><?php the_excerpt(); ?></p>
 							<p class="released-date"><?php echo get_the_date(); ?></p>
 						</a>
 					</article>
 
-					<?php
-						endwhile;
-					else:
-					?>
+							<?php
+								endwhile;
+							else :
+								?>
 
 					<p>記事はまだないみたいだ。</p>
 
-					<?php
-					endif;
-					?>
+								<?php
+								endif;
+							?>
 
 					<nav class="page-nav">
 						<ul>
