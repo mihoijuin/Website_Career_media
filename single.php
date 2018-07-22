98f9fc42575c34a395e781a02b1c5446c72d8a3b
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-07-22 23:38:10 +0900

    スペースをタブに修正

--- single.php
+++ single.php
@@ -13,59 +13,59 @@
 ?>
 <?php get_header(); ?>
 
-    <!-- single -->
-    <div class="single">
-      <div class="single-content">
-        <img src="http://placehold.jp/1000x350.png" alt="" width="100%">
-        <div class="single-post">
-          <header>
-            <h2 class="single-title">記事タイトル</h2>
-            <p class="single-category">カテゴリ</p>
-          </header>
-          <p class="txt-content">
-            記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
-            記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
-            記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
-            記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
-            記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
-            記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
-            記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
-            記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
-            記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
-          </p>
-          <p class="released-date">2018/07/08</p>
-          <div class="share">
-              <ul>
-                <li><a href=""><span class="fab fa-twitter"></span></a></li>
-                <li><a href=""><span class="fab fa-facebook"></span></a></li>
-              </ul>
-          </div>
-        </div>
-      </div>
+		<!-- single -->
+		<div class="single">
+			<div class="single-content">
+				<img src="http://placehold.jp/1000x350.png" alt="" width="100%">
+				<div class="single-post">
+					<header>
+						<h2 class="single-title">記事タイトル</h2>
+						<p class="single-category">カテゴリ</p>
+					</header>
+					<p class="txt-content">
+						記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
+						記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
+						記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
+						記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
+						記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
+						記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
+						記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
+						記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
+						記事本文。だあああああああああああああああああああああああああああああああああああああああああああああああああああああ
+					</p>
+					<p class="released-date">2018/07/08</p>
+					<div class="share">
+						<ul>
+							<li><a href=""><span class="fab fa-twitter"></span></a></li>
+							<li><a href=""><span class="fab fa-facebook"></span></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
 
-      <!-- sidebar -->
-      <div class="sidebar">
-        <section class="side-content">
-          <h3>人気記事</h3>
-          <ul>
-            <li><a href="single.html">記事タイトル</a></li>
-            <li><a href="single.html">記事タイトル</a></li>
-            <li><a href="single.html">記事タイトル</a></li>
-            <li><a href="single.html">記事タイトル</a></li>
-            <li><a href="single.html">記事タイトル</a></li>
-          </ul>
-        </section>
-        <section class="side-content">
-          <h3>最新記事</h3>
-          <ul>
-            <li><a href="single.html">記事タイトル</a></li>
-            <li><a href="single.html">記事タイトル</a></li>
-            <li><a href="single.html">記事タイトル</a></li>
-            <li><a href="single.html">記事タイトル</a></li>
-            <li><a href="single.html">記事タイトル</a></li>
-          </ul>
-        </section>
-      </div>
-    </div>
+			<!-- sidebar -->
+			<div class="sidebar">
+				<section class="side-content">
+					<h3>人気記事</h3>
+					<ul>
+						<li><a href="single.html">記事タイトル</a></li>
+						<li><a href="single.html">記事タイトル</a></li>
+						<li><a href="single.html">記事タイトル</a></li>
+						<li><a href="single.html">記事タイトル</a></li>
+						<li><a href="single.html">記事タイトル</a></li>
+					</ul>
+				</section>
+				<section class="side-content">
+					<h3>最新記事</h3>
+					<ul>
+						<li><a href="single.html">記事タイトル</a></li>
+						<li><a href="single.html">記事タイトル</a></li>
+						<li><a href="single.html">記事タイトル</a></li>
+						<li><a href="single.html">記事タイトル</a></li>
+						<li><a href="single.html">記事タイトル</a></li>
+					</ul>
+				</section>
+			</div>
+		</div>
 
 <?php get_footer(); ?>
