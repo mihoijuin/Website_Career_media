6d9a551963faed4b2afcfff65488d13fb5fd29e5
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-07-22 23:25:04 +0900

    スペースをタブに修正

--- profile.php
+++ profile.php
@@ -12,33 +12,33 @@
 
 ?>
 <?php get_header(); ?>
-    <!-- main -->
-    <main>
-      <div class="main-wrapper">
-        <img class="heading" src="./images/profile-title.svg" alt="">
-        <div class="profiles">
-          <section class="profile">
-            <h3>ぽりん</h3>
-            <img src="./images/porin-wanko.png" alt="" width="200" height="200">
-            <p>
-              自己紹介的なものをだいたい<br>
-              3行くらいで書くのがいいかな<br>
-              こんな人ですよ〜〜
-            </p>
-            <a href="https://twitter.com/iju_miho" target="_blank"><span class="fab fa-twitter"></span></a>
-          </section>
-          <section class="profile">
-            <h3>けんと</h3>
-            <img src="./images/kento-nyanko.png" alt="" width="200" height="200">
-            <p>
-              自己紹介的なものをだいたい<br>
-              3行くらいで書くのがいいかな<br>
-              こんな人ですよ〜〜
-            </p>
-            <a href="https://twitter.com/kntaoki" target="_blank"><span class="fab fa-twitter"></span></a>
-          </section>
-        </div>
-      </div>
-    </main>
+		<!-- main -->
+		<main>
+			<div class="main-wrapper">
+				<img class="heading" src="./images/profile-title.svg" alt="">
+				<div class="profiles">
+					<section class="profile">
+						<h3>ぽりん</h3>
+						<img src="./images/porin-wanko.png" alt="" width="200" height="200">
+						<p>
+							自己紹介的なものをだいたい<br>
+							3行くらいで書くのがいいかな<br>
+							こんな人ですよ〜〜
+						</p>
+						<a href="https://twitter.com/iju_miho" target="_blank"><span class="fab fa-twitter"></span></a>
+					</section>
+					<section class="profile">
+						<h3>けんと</h3>
+						<img src="./images/kento-nyanko.png" alt="" width="200" height="200">
+						<p>
+							自己紹介的なものをだいたい<br>
+							3行くらいで書くのがいいかな<br>
+							こんな人ですよ〜〜
+						</p>
+						<a href="https://twitter.com/kntaoki" target="_blank"><span class="fab fa-twitter"></span></a>
+					</section>
+				</div>
+			</div>
+			</main>
 
 <?php get_footer(); ?>
