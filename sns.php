43d2c2749ea862f42315fb6de57c8b4b24ce17f0
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-07-23 15:41:07 +0900

    消えていたdivの閉じタグを修正

--- sns.php
+++ sns.php
@@ -35,3 +35,4 @@ $title_encode = rawurlencode( get_the_title() ) . '｜' . get_bloginfo( 'name' )
 			<a data-pocket-label="pocket" data-pocket-count="none" class="pocket-btn" data-lang="en"></a>
 			<script type="text/javascript">!function(d,i){if(!d.getElementById(i)){var j=d.createElement("script");j.id=i;j.src="https://widgets.getpocket.com/v1/j/btn.js?v=1";var w=d.getElementById(i);d.body.appendChild(j);}}(document,"pocket-btn-js");</script>
 		</li>
+</div>
