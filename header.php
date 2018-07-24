d3f6f7cd50d0665ddf2e41f40221370be74f5891
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-07-24 20:38:50 +0900

    アイコンをヘッダーに追加

--- header.php
+++ header.php
@@ -32,7 +32,7 @@
 		<header id="header">
 			<section class="header-logo">
 				<a href="<?php echo home_url(); ?>">
-				<img class="header-icon" src="<?php echo get_template_directory_uri(); ?>/images/Porin_icon.jpg" alt="" width="120" height="120">
+				<img class="header-icon" src="<?php echo get_template_directory_uri(); ?>/images/porin-wanko.png" alt="" width="100" height="100">
 				</a>
 				<div class="media-title">
 					<h1>
@@ -43,6 +43,7 @@
 						「なんで？」「でも〜」「だから〜」が飛び交う場
 					</p>
 				</div>
+				<img class="header-icon" src="<?php echo get_template_directory_uri(); ?>/images/kento-nyanko.png" alt="" width="100" height="100">
 			</section>
 			<nav class="header-nav">
 				<?php wp_nav_menu(); ?>
