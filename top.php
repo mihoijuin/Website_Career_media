ad0b5db88201248672d17523755213ca85bc7b82
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-07-24 11:45:44 +0900

    インデントを調整

--- top.php
+++ top.php
@@ -9,17 +9,6 @@
  * @subpackage Nandedemodakara
  * @since 1.0
  * @version 1.0
- *
- * <article class="popular-post">
- * 	<a href="single.html">
- *		<header>
- *			<h3 class="popular-title">記事タイトル</h3>
- *			<p class="popular-category">カテゴリ</p>
- *		</header>
- *		<p>記事の冒頭部分がここに来るよおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお</p>
- *		<p class="released-date">2018/07/08</p>
- *	</a>
- *</article>
  */
 
 ?>
@@ -33,31 +22,31 @@
 				<!-- 人気記事 -->
 				<?php
 					$arg = array (
-						'post_type' => 'post',
-						'limit' => 3,
-						'range' => 'monthry',
-						'order_by' => 'views',
-						'stats_views' => 0, //view数を表示しない
-						'stats_category' => 1,
-						'stats_date' => 1,
-						'excerpt_length' => 20,
-						'thumbnail_width' => 300,
+						'post_type'        => 'post',
+						'limit'            => 3,
+						'range'            => 'monthry',
+						'order_by'         => 'views',
+						'stats_views'      => 0, // view数を表示しない.
+						'stats_category'   => 1,
+						'stats_date'       => 1,
+						'excerpt_length'   => 20,
+						'thumbnail_width'  => 300,
 						'thumbnail_height' => 300,
-						'wpp_start' => '<div class="popular-posts">',
-						'wpp_end' => '</div>',
-						'post_html' => '<article class="popular-post">
-															<a href="{url}">
-																{thumb_img}
-																<header>
-																	<h3 class="popular-title">{text_title}</h3>
-																	<p class="popular-category">{category}</p>
-																</header>
-																<p class="popular-txt">{summary}</p>
-																<p class="released-date">{date}</p>
-															</a>
-														</article>',
+						'wpp_start'        => '<div class="popular-posts">',
+						'wpp_end'          => '</div>',
+						'post_html'        => '<article class="popular-post">
+																		<a href="{url}">
+																			{thumb_img}
+																			<header>
+																				<h3 class="popular-title">{text_title}</h3>
+																				<p class="popular-category">{category}</p>
+																			</header>
+																			<p class="popular-txt">{summary}</p>
+																			<p class="released-date">{date}</p>
+																		</a>
+																	</article>',
 					);
-					wpp_get_mostpopular($arg);
+					wpp_get_mostpopular( $arg );
 					?>
 
 
