2e0996ec6f9607329fe8971ee4d1b4c8e9348b16
Miho Ijuin <miiiiiiiii747@gmail.com>
2018-07-24 14:50:06 +0900

    snsボタンの記述を修正

--- sns.php
+++ sns.php
@@ -15,7 +15,7 @@
 	<ul>
 		<!--ツイートボタン-->
 		<li class="tweet">
-			<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a>
+			<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false"></a>
 		</li>
 
 		<!--はてなボタン-->
